3dd13dfd03f4544500d67ca408bf7793b7e81688
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2023-01-09 23:02:07 +0100

    11. desember, del 2

--- src/december_11.rs
+++ src/december_11.rs
@@ -1,50 +1,15 @@
 use std::collections::VecDeque;
 
-pub fn part_1(input: &str) -> u32 {
-    let monkey_strings = input.split("\n\n");
-    let mut monkeys = Vec::<Monkey>::new();
-    for monkey_string in monkey_strings {
-        monkeys.push(Monkey::new(monkey_string));
-    }
-
-    for _ in 0..20 {
-        for monkey in 0..monkeys.len() {
-            //println!("Monkey {}:", monkey);
-            for _ in 0..monkeys[monkey].items.len() {
-                let mut worry_level = monkeys[monkey].inspect_item();
-                //monkeys[monkey].print_inspection(worry_level);
-
-                let true_monkey = monkeys[monkey].next_monkey.0;
-                let false_monkey = monkeys[monkey].next_monkey.1;
-
-                worry_level = monkeys[monkey].operation(worry_level);
-                worry_level /= 3;
-
-                match worry_level % monkeys[monkey].divisor {
-                    0 => monkeys[true_monkey].catch_item(worry_level),
-                    _ => monkeys[false_monkey].catch_item(worry_level),
-                }
-                monkeys[monkey].inspected += 1;
-            }
-        }
-    }
-
-    let mut inspected = monkeys.iter().map(|m| m.inspected).collect::<Vec<u32>>();
-    inspected.sort();
-    inspected.pop().unwrap() * inspected.pop().unwrap()
-}
-
-pub fn part_2(input: &str) -> u32 {
-    2
-}
+extern crate num;
+use num::integer::lcm;
 
 struct Monkey {
-    pub items: VecDeque<u32>,
+    pub items: VecDeque<u64>,
     pub operator: char,
     pub operand: String,
     pub divisor: u32,
     pub next_monkey: (usize, usize),
-    pub inspected: u32,
+    pub inspected: u64,
 }
 
 impl Monkey {
@@ -53,11 +18,11 @@ impl Monkey {
         lines.next().unwrap();
 
         let (_, item_string) = lines.next().unwrap().split_once(": ").unwrap();
-        let items: VecDeque<u32> = match item_string.contains(',') {
-            true => item_string.split(", ").map(|s| s.parse::<u32>().unwrap()).collect::<VecDeque<u32>>(),
+        let items: VecDeque<u64> = match item_string.contains(',') {
+            true => item_string.split(", ").map(|s| s.parse::<u64>().unwrap()).collect::<VecDeque<u64>>(),
             false => {
                 let mut new_vec_deque = VecDeque::new();
-                new_vec_deque.push_back(item_string.parse::<u32>().unwrap());
+                new_vec_deque.push_back(item_string.parse::<u64>().unwrap());
                 new_vec_deque
             }
         };
@@ -76,12 +41,12 @@ impl Monkey {
             operator,
             operand,
             divisor: divisor.parse::<u32>().unwrap(),
-            next_monkey: (true_monkey.parse::<u32>().unwrap() as usize, false_monkey.parse::<u32>().unwrap() as usize),
+            next_monkey: (true_monkey.parse::<usize>().unwrap(), false_monkey.parse::<usize>().unwrap()),
             inspected: 0
         }
     }
 
-    pub fn operation(&self, old: u32) -> u32 {
+    pub fn operation(&self, old: u64) -> u64 {
         match self.operand.as_str() {
             "old" => {
                 match self.operator {
@@ -91,55 +56,142 @@ impl Monkey {
             }
             x => {
                 match self.operator {
-                    '*' => old * x.parse::<u32>().unwrap(),
-                    _ => old + x.parse::<u32>().unwrap()
+                    '*' => old * x.parse::<u64>().unwrap(),
+                    _ => old + x.parse::<u64>().unwrap()
                 }
             }
         }
     }
 
-    pub fn inspect_item(&mut self) -> u32 {
+    pub fn inspect_item(&mut self) -> u64 {
         self.items.pop_front().unwrap()
     }
 
-    pub fn catch_item(&mut self, item: u32) {
-        self.items.push_back(item);
+    pub fn catch_item(&mut self, worry_level: u64) {
+        self.items.push_back(worry_level);
+    }
+}
+
+pub fn part_1(input: &str) -> u64 {
+    let monkey_strings = input.split("\n\n");
+    let mut monkeys = Vec::<Monkey>::new();
+    for monkey_string in monkey_strings {
+        monkeys.push(Monkey::new(monkey_string));
     }
 
-    pub fn print_inspection(&self, worry_level: u32) {
-        println!("  Monkey inspects an item with a worry level of {}.", worry_level);
+    for _ in 0..20 {
+        for monkey in 0..monkeys.len() {
+            //println!("Monkey {}:", monkey);
+            for _ in 0..monkeys[monkey].items.len() {
+                let mut worry_level = monkeys[monkey].inspect_item();
+                print_inspection(&monkeys[monkey],worry_level);
 
-        let true_monkey = self.next_monkey.0;
-        let false_monkey = self.next_monkey.1;
+                let true_monkey = monkeys[monkey].next_monkey.0;
+                let false_monkey = monkeys[monkey].next_monkey.1;
 
-        let mut worry_level = self.operation(worry_level);
-        let operand = match self.operand.as_str() {
-            "old" => worry_level,
-            x => x.parse::<u32>().unwrap()
-        };
-        match self.operator {
-            '*' => {
-                println!("    Worry level is multiplied by {} to {}.", operand,  worry_level);
-            },
-            _ => {
-                println!("    Worry level is increased by {} to {}.", operand,  worry_level);
+                worry_level = monkeys[monkey].operation(worry_level);
+                worry_level /= 3;
+
+                match worry_level % monkeys[monkey].divisor as u64 {
+                    0 => monkeys[true_monkey].catch_item(worry_level),
+                    _ => monkeys[false_monkey].catch_item(worry_level),
+                }
+                monkeys[monkey].inspected += 1;
+            }
+        }
+    }
+
+    let mut inspected = monkeys.iter().map(|m| m.inspected).collect::<Vec<u64>>();
+    inspected.sort();
+    inspected.pop().unwrap() * inspected.pop().unwrap()
+}
+
+fn print_inspection(monkey: &Monkey, worry_level: u64) {
+    println!("  Monkey inspects an item with a worry level of {}.", worry_level);
+
+    let true_monkey = monkey.next_monkey.0;
+    let false_monkey = monkey.next_monkey.1;
+
+    let mut worry_level = monkey.operation(worry_level);
+    let operand = match monkey.operand.as_str() {
+        "old" => worry_level,
+        x => x.parse::<u64>().unwrap()
+    };
+    match monkey.operator {
+        '*' => {
+            println!("    Worry level is multiplied by {} to {}.", operand,  worry_level);
+        },
+        _ => {
+            println!("    Worry level is increased by {} to {}.", operand,  worry_level);
+        }
+    }
+
+    worry_level /= 3;
+    println!("    Monkey gets bored with item. Worry level is divided by 3 to {}.",  worry_level);
+
+    match worry_level % monkey.divisor as u64 {
+        0 => {
+            println!("    Current worry level is divisible by {}.", monkey.divisor);
+            println!("    Item with worry level {} is thrown to monkey {}.", worry_level, true_monkey);
+        },
+        _ => {
+            println!("    Current worry level is not divisible by {}.", monkey.divisor);
+            println!("    Item with worry level {} is thrown to monkey {}.", worry_level, false_monkey);
+        },
+    }
+}
+
+pub fn part_2(input: &str) -> u64 {
+    let monkey_strings = input.split("\n\n");
+    let mut monkeys = Vec::<Monkey>::new();
+    for monkey_string in monkey_strings {
+        monkeys.push(Monkey::new(monkey_string));
+    }
+    let lowest_common_multiple = get_lowest_common_multiple(&monkeys);
+
+    for round in 1..=10000 {
+        for monkey in 0..monkeys.len() {
+            for _ in 0..monkeys[monkey].items.len() {
+                let mut worry_level = monkeys[monkey].inspect_item();
+
+                let true_monkey = monkeys[monkey].next_monkey.0;
+                let false_monkey = monkeys[monkey].next_monkey.1;
+
+                worry_level = monkeys[monkey].operation(worry_level);
+                worry_level %= lowest_common_multiple;
+
+                match worry_level % monkeys[monkey].divisor as u64 {
+                    0 => monkeys[true_monkey].catch_item(worry_level),
+                    _ => monkeys[false_monkey].catch_item(worry_level),
+                }
+                monkeys[monkey].inspected += 1;
             }
         }
 
-        worry_level /= 3;
-        println!("    Monkey gets bored with item. Worry level is divided by 3 to {}.",  worry_level);
-
-        match worry_level % self.divisor {
-            0 => {
-                println!("    Current worry level is divisible by {}.", self.divisor);
-                println!("    Item with worry level {} is thrown to monkey {}.", worry_level, true_monkey);
-            },
-            _ => {
-                println!("    Current worry level is not divisible by {}.", self.divisor);
-                println!("    Item with worry level {} is thrown to monkey {}.", worry_level, false_monkey);
-            },
+        if round == 1 || round == 20 || round % 1000 == 0 {
+            print_inspected(&monkeys, round);
         }
     }
+
+    let mut inspected = monkeys.iter().map(|m| m.inspected).collect::<Vec<u64>>();
+    inspected.sort();
+    inspected.pop().unwrap() * inspected.pop().unwrap()
+}
+
+fn get_lowest_common_multiple(monkeys: &[Monkey]) -> u64 {
+    let mut multiple: u64 = 1;
+    for monkey in monkeys {
+        multiple = lcm(multiple, monkey.divisor as u64);
+    }
+    multiple
+}
+
+fn print_inspected(monkeys: &[Monkey], round: u32) {
+    println!("== After round {} ==", round);
+    for (i, monkey) in monkeys.iter().enumerate() {
+        println!("Monkey {} inspected items {} times.", i, monkey.inspected);
+    }
+    println!();
 }
 
 #[test]
@@ -151,5 +203,5 @@ fn sample_input_part_1() {
 #[test]
 fn sample_input_part_2() {
     let input = include_str!("../input/sample_11.txt");
-    assert_eq!(part_2(input), 2)
+    assert_eq!(part_2(input), 2713310158)
 }
