4cdd34c6a9f1380d30064a9adc7f993ea0b7dab0
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2023-12-04 13:17:06 +0100

    HashSet med tall, HashMap med Flax-lodd som skal skrapes

--- 2023/src/december_4.rs
+++ 2023/src/december_4.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashSet, HashMap};
+
 pub fn part_1(input: &str) -> i32 {
     input.lines()
         .map(to_card)
@@ -8,16 +10,13 @@ pub fn part_1(input: &str) -> i32 {
 #[derive(Clone, Debug)]
 struct Card {
     number: i16,
-    winning: Vec<i8>,
-    scratched: Vec<i8>,
+    winning: HashSet<i8>,
+    scratched: HashSet<i8>,
 }
 
 impl Card {
     fn get_num_winning(&self) -> usize {
-        self.scratched
-            .iter()
-            .filter(|&num| self.winning.contains(&num))
-            .count()
+        self.scratched.intersection(&self.winning).count()
     }
     fn calculate_points(&self) -> i32 {
         match self.get_num_winning() {
@@ -25,10 +24,10 @@ impl Card {
             n => 2_i32.pow(n as u32 - 1)
         }
     }
-    fn get_new_cards(&self) -> Option<Vec<i16>> {
+    fn get_new_cards(&self) -> Vec<i16> {
         match self.get_num_winning() {
-            0 => None,
-            n => Some((self.number+1..self.number+1+n as i16).collect())
+            0 => Vec::new(),
+            n => (self.number+1..self.number+1+n as i16).collect()
         }
     }
 }
@@ -36,38 +35,38 @@ impl Card {
 fn to_card(input: &str) -> Card {
     let (card_str, numbers) = input.split_once(':').unwrap();
     let card_num = card_str.split_once(' ').unwrap().1.trim();
-    let lists = to_two_lists(numbers);
+    let lists = to_two_sets(numbers);
 
     Card { number: card_num.parse::<i16>().unwrap(), winning: lists.0, scratched: lists.1 }
 }
 
-fn to_two_lists(input: &str) -> (Vec<i8>, Vec<i8>) {
-    let lists: Vec<Vec<i8>> = input
+fn to_two_sets(input: &str) -> (HashSet<i8>, HashSet<i8>) {
+    let lists: Vec<HashSet<i8>> = input
         .split('|')
         .map(|number_str| number_str
             .split_whitespace()
             .map(|str| str.parse::<i8>().unwrap())
             .collect())
         .collect();
-    (lists.get(0).unwrap().to_vec(), lists.get(1).unwrap().to_vec())
+    (HashSet::from_iter(lists.get(0).unwrap().iter().cloned()), HashSet::from_iter(lists.get(1).unwrap().iter().cloned()))
 }
 
 pub fn part_2(input: &str) -> i32 {
     let mut cards_scratched = 0;
     let cards: Vec<Card> = input.lines().map(to_card).collect();
-    let mut cards_to_scratch = Vec::<Card>::new();
+    let card_to_new_cards: HashMap<i16, Vec<i16>> = cards
+        .iter()
+        .map(|card| (card.number, card.get_new_cards())).collect();
+    let mut cards_to_scratch = Vec::<i16>::new();
 
     for card in cards.iter() {
-        cards_to_scratch.push(card.clone());
+        cards_to_scratch.push(card.number);
     }
 
-    while !cards_to_scratch.is_empty() {
-        let card = cards_to_scratch.pop().unwrap();
+    while let Some(card_number) = cards_to_scratch.pop() {
         cards_scratched += 1;
-        if let Some(new_cards) = card.get_new_cards() {
-            for new_card_number in new_cards {
-                cards_to_scratch.push(cards.get(new_card_number as usize - 1).unwrap().clone());
-            }
+        for new_card_number in card_to_new_cards.get_key_value(&card_number).unwrap().1 {
+            cards_to_scratch.push(*new_card_number);
         }
     }
     cards_scratched
