7430383eeccc4ae7ec3dafc0f15a00fee833cb51
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2023-12-07 12:59:51 +0100

    Luke 7, del 2

--- 2023/src/december_7.rs
+++ 2023/src/december_7.rs
@@ -11,6 +11,16 @@ pub fn part_1(input: &str) -> i32 {
         .sum()
 }
 
+pub fn part_2(input: &str) -> i32 {
+    input.lines()
+        .map(|line| line.split_once(' ').unwrap())
+        .map(|(card_str, bid)| (Hand::new_with_joker(card_str), bid.parse::<i32>().unwrap()))
+        .sorted_by(|(hand_1, _), (hand_2, _)| hand_1.partial_cmp(hand_2).unwrap())
+        .enumerate()
+        .map(|(index, (_, bid))| (index as i32 + 1) * bid )
+        .sum()
+}
+
 struct Hand {
     cards: Vec<Card>,
 }
@@ -21,18 +31,26 @@ impl Hand {
             cards: card_str.chars().map(Card::new).collect()
         }
     }
+    pub fn new_with_joker(card_str: &str) -> Hand {
+        Hand {
+            cards: card_str.chars().map(Card::new).map(|card| match card {
+                Card::Jack => Card::Joker,
+                _ => card
+            }).collect()
+        }
+    }
     fn get_type(&self) -> HandType {
-        if self.cards.iter().all_equal() {
+        if self.most_frequent_card().1 == 5 {
             HandType::FiveOfAKind
         } else if self.most_frequent_card().1 == 4 {
             HandType::FourOfAKind
         } else if self.most_frequent_card().1 == 3 && self.second_most_frequent_card().1 == 2 {
             HandType::FullHouse
-        } else if self.most_frequent_card().1 == 3 && self.cards.iter().unique().count() == 3 {
+        } else if self.most_frequent_card().1 == 3 && self.unique() == 3 {
             HandType::ThreeOfAKind
-        } else if self.most_frequent_card().1 == 2 && self.second_most_frequent_card().1 == 2 && self.cards.iter().unique().count() == 3 {
-            HandType::TwoPairs
-        } else if self.most_frequent_card().1 == 2 && self.second_most_frequent_card().1 == 1 && self.cards.iter().unique().count() == 4 {
+        } else if self.most_frequent_card().1 == 2 && self.second_most_frequent_card().1 == 2 && self.unique() == 3 {
+            HandType::TwoPair
+        } else if self.most_frequent_card().1 == 2 && self.second_most_frequent_card().1 == 1 && self.unique() == 4 {
             HandType::OnePair
         } else {
             HandType::HighCard
@@ -42,13 +60,14 @@ impl Hand {
         let mut most_frequent_card = None;
         let mut most_frequent_card_count = 0;
         for card in self.cards.iter().unique() {
+            if *card == Card::Joker { continue }
             let card_count = self.cards.iter().filter(|&c| c == card).count();
             if card_count > most_frequent_card_count {
                 most_frequent_card = Some(card);
                 most_frequent_card_count = card_count;
             }
         }
-        (most_frequent_card, most_frequent_card_count)
+        (most_frequent_card, most_frequent_card_count + self.jokers())
     }
     fn second_most_frequent_card(&self) -> (Option<&Card>, usize) {
         let (most_frequent_card_opt, count) = self.most_frequent_card();
@@ -59,6 +78,7 @@ impl Hand {
                 let mut second_most_frequent_card = None;
                 let mut second_most_frequent_card_count = 0;
                 for card in self.cards.iter().unique() {
+                    if *card == Card::Joker { continue }
                     let card_count = self.cards.iter().filter(|&c| c == card).count();
                     if card_count > second_most_frequent_card_count && card != most_frequent_card {
                         second_most_frequent_card = Some(card);
@@ -68,6 +88,12 @@ impl Hand {
                 (second_most_frequent_card, second_most_frequent_card_count)
             }
         }
+    }
+    fn unique(&self) -> usize {
+        self.cards.iter().unique().count() - (self.jokers() > 0) as usize
+    }
+    fn jokers(&self) -> usize {
+        self.cards.iter().filter(|&card| *card == Card::Joker).count()
     }
 }
 
@@ -93,6 +119,7 @@ impl PartialOrd for Hand {
 
 #[derive(PartialEq, PartialOrd, Eq, Hash)]
 enum Card {
+    Joker, // Verdt minst som enkeltkort
     Two,
     Three,
     Four,
@@ -129,20 +156,15 @@ impl Card {
     }
 }
 
-#[derive(PartialEq, PartialOrd)]
+#[derive(PartialEq, PartialOrd, Debug)]
 enum HandType {
     HighCard,
     OnePair,
-    TwoPairs,
+    TwoPair,
     ThreeOfAKind,
     FullHouse,
     FourOfAKind,
     FiveOfAKind,
-}
-
-pub fn part_2(input: &str) -> i16 {
-
-    0
 }
 
 #[test]
@@ -154,5 +176,115 @@ fn sample_input_part_1() {
 #[test]
 fn sample_input_part_2() {
     let input = include_str!("../input/sample_7.txt");
-    assert_eq!(part_2(input), 0)
+    assert_eq!(part_2(input), 5905)
+}
+
+#[test]
+fn five_of_a_kind() {
+    let hand = Hand::new("KKKKK");
+    assert_eq!(hand.get_type(), HandType::FiveOfAKind)
+}
+
+#[test]
+fn five_of_a_kind_with_jokers() {
+    let hand_with_one_joker = Hand::new_with_joker("KKKJK");
+    assert_eq!(hand_with_one_joker.get_type(), HandType::FiveOfAKind);
+    let hand_with_two_jokers = Hand::new_with_joker("KKKJJ");
+    assert_eq!(hand_with_two_jokers.get_type(), HandType::FiveOfAKind);
+    let hand_with_three_jokers = Hand::new_with_joker("KKJJJ");
+    assert_eq!(hand_with_three_jokers.get_type(), HandType::FiveOfAKind);
+    let hand_with_four_jokers = Hand::new_with_joker("KJJJJ");
+    assert_eq!(hand_with_four_jokers.get_type(), HandType::FiveOfAKind);
+    let hand_with_five_jokers = Hand::new_with_joker("JJJJJ");
+    assert_eq!(hand_with_five_jokers.get_type(), HandType::FiveOfAKind)
+}
+
+#[test]
+fn four_of_a_kind() {
+    let hand = Hand::new("K2KKK");
+    assert_eq!(hand.get_type(), HandType::FourOfAKind)
+}
+
+#[test]
+fn four_of_a_kind_with_jokers() {
+    let hand_with_one_joker = Hand::new_with_joker("K2KJK");
+    assert_eq!(hand_with_one_joker.get_type(), HandType::FourOfAKind);
+    let hand_with_two_jokers = Hand::new_with_joker("K2KJJ");
+    assert_eq!(hand_with_two_jokers.get_type(), HandType::FourOfAKind);
+    let hand_with_three_jokers = Hand::new_with_joker("K2JJJ");
+    assert_eq!(hand_with_three_jokers.get_type(), HandType::FourOfAKind);
+
+    // Endrer type ved tilstrekkelig antall jokere
+    let hand_with_four_jokers = Hand::new_with_joker("KJJJJ");
+    assert_eq!(hand_with_four_jokers.get_type(), HandType::FiveOfAKind)
+}
+
+#[test]
+fn full_house() {
+    let hand = Hand::new("K22KK");
+    assert_eq!(hand.get_type(), HandType::FullHouse)
+}
+
+#[test]
+fn full_house_with_jokers() {
+    let hand_with_one_joker = Hand::new_with_joker("K22JK");
+    assert_eq!(hand_with_one_joker.get_type(), HandType::FullHouse);
+
+    // Endrer type ved tilstrekkelig antall jokere
+    let hand_with_two_jokers = Hand::new_with_joker("K22JJ");
+    assert_eq!(hand_with_two_jokers.get_type(), HandType::FourOfAKind)
+}
+
+#[test]
+fn three_of_a_kind() {
+    let hand = Hand::new("K29KK");
+    assert_eq!(hand.get_type(), HandType::ThreeOfAKind)
+}
+
+#[test]
+fn three_of_a_kind_with_jokers() {
+    let hand_with_one_joker = Hand::new_with_joker("K29JK");
+    assert_eq!(hand_with_one_joker.get_type(), HandType::ThreeOfAKind);
+    let hand_with_two_jokers = Hand::new_with_joker("K29JJ");
+    assert_eq!(hand_with_two_jokers.get_type(), HandType::ThreeOfAKind);
+}
+
+#[test]
+fn two_pair() {
+    let hand = Hand::new("KK229");
+    assert_eq!(hand.get_type(), HandType::TwoPair)
+}
+
+#[test]
+fn two_pair_with_joker() {
+    // Får aldri to par med jokere
+    let hand_with_one_joker = Hand::new_with_joker("KK2J9");
+    assert_eq!(hand_with_one_joker.get_type(), HandType::ThreeOfAKind);
+    let hand_with_two_jokers = Hand::new_with_joker("K2JJ9");
+    assert_eq!(hand_with_two_jokers.get_type(), HandType::ThreeOfAKind)
+}
+
+#[test]
+fn one_pair() {
+    let hand = Hand::new("KK229");
+    assert_eq!(hand.get_type(), HandType::TwoPair)
+}
+
+#[test]
+fn one_pair_with_joker() {
+    let hand_with_one_joker = Hand::new_with_joker("K234J");
+    assert_eq!(hand_with_one_joker.get_type(), HandType::OnePair);
+}
+
+#[test]
+fn high_card() {
+    let hand = Hand::new("K2345");
+    assert_eq!(hand.get_type(), HandType::HighCard)
+}
+
+#[test]
+fn high_card_with_joker() {
+    // Får aldri høyt kort med joker
+    let hand_with_one_joker = Hand::new_with_joker("K234J");
+    assert_eq!(hand_with_one_joker.get_type(), HandType::OnePair);
 }
