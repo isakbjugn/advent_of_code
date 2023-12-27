b0f5c73fc78e0182581f505d2a103851f57ce45e
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2023-12-28 00:49:48 +0100

    Luke 15, del 2

--- 2023/src/december_15.rs
+++ 2023/src/december_15.rs
@@ -1,7 +1,10 @@
+use std::collections::hash_map::Entry::Vacant;
+use std::collections::HashMap;
 use memoize::memoize;
+use crate::sum_to::SumTo;
 
 pub fn part_1(input: &str) -> u32 {
-    input.split(',').map(hash).map(|h| h as u32).sum()
+    input.split(',').map(hash).sum_to()
 }
 
 fn hash(string: &str) -> u8 {
@@ -18,8 +21,44 @@ fn normalise(value: u16) -> u8 {
 }
 
 pub fn part_2(input: &str) -> u32 {
+    let mut hashmap: HashMap<u8, Vec<(&str, u8)>> = HashMap::new();
+    for string in input.split(',') {
+        match string.contains('=') {
+            true => {
+                let (label, focal_length_str) = string.split_once('=').unwrap();
+                let lens_box = hash(label);
+                let focal_length = focal_length_str.parse().unwrap();
+                if let Vacant(e) = hashmap.entry(lens_box) {
+                    e.insert(vec![(label, focal_length)]);
+                } else {
+                    match hashmap.get(&lens_box).unwrap().iter().position(|&(l, _)| l == label) {
+                        Some(idx) => hashmap.get_mut(&lens_box).unwrap()[idx] = (label, focal_length),
+                        None => hashmap.get_mut(&lens_box).unwrap().push((label, focal_length))
+                    }
+                }
+            }
+            false => {
+                let (label, _) = string.split_once('-').unwrap();
+                let lens_box = hash(label);
+                if hashmap.contains_key(&lens_box) {
+                    if let Some(idx) = hashmap.get(&lens_box).unwrap().iter().position(|&(l, _)| l == label) {
+                        hashmap.get_mut(&lens_box).unwrap().remove(idx);
+                    }
+                }
+            }
+        }
+    }
+    hashmap.iter().map(|(&box_idx, box_content)|
+        box_content.iter().enumerate()
+            .map(|(slot, &(_, focal_length))|
+                focusing_power(box_idx, slot, focal_length)
+            )
+            .sum::<u16>()
+    ).sum_to()
+}
 
-    0
+fn focusing_power(box_number: u8, slot: usize, focal_length: u8) -> u16 {
+    (box_number as u16 + 1) * (slot as u16 + 1) * focal_length as u16
 }
 
 #[test]
@@ -37,5 +76,5 @@ fn sample_input_part_1() {
 #[test]
 fn sample_input_part_2() {
     let input = include_str!("../input/sample_15.txt");
-    assert_eq!(part_2(input), 0)
+    assert_eq!(part_2(input), 145)
 }
