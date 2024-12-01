52a846c22fba3f66758eb813cd3c62163f913241
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-12-01 10:12:07 +0100

    1. desember, del 2

--- 2024/src/december_1.rs
+++ 2024/src/december_1.rs
@@ -13,7 +13,19 @@ pub fn part_1(input: &str) -> u32 {
 }
 
 pub fn part_2(input: &str) -> u32 {
-    0
+    let (mut first_list, mut second_list): (Vec<u32>, Vec<u32>) = input
+        .lines()
+        .map(|line| line.split_once("   ").unwrap())
+        .map(|(a, b)| (a.parse::<u32>().unwrap(), b.parse::<u32>().unwrap()))
+        .unzip();
+    first_list.sort();
+    second_list.sort();
+    
+    first_list
+        .iter()
+        .map(|a| (a, second_list.iter().filter(|&b| a == b).count() as u32))
+        .map(|(a, n)| a * n)
+        .sum()
 }
 
 #[test]
@@ -25,5 +37,5 @@ fn sample_input_part_1() {
 #[test]
 fn sample_input_part_2() {
     let input = include_str!("../input/sample_1.txt");
-    assert_eq!(part_2(input), 0)
+    assert_eq!(part_2(input), 31)
 }
