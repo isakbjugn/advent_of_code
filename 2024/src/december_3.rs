0cfa74eb66b96f26b2286da08dd8f648ad39ecbd
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-12-03 07:00:37 +0100

    3. desember, del 1

--- 2024/src/december_3.rs
+++ 2024/src/december_3.rs
@@ -1,5 +1,19 @@
-pub fn part_1(input: &str) -> usize {
-    0
+use regex::Regex;
+
+pub fn part_1(input: &str) -> u32 {
+    let pattern = r"mul\((\d+),(\d+)\)";
+
+    // Compile the regular expression
+    let re = Regex::new(pattern).unwrap();
+    let mut sum = 0;
+
+    // Find all matches
+    for caps in re.captures_iter(input) {
+        let num1 = caps.get(1).unwrap().as_str().parse::<u32>().unwrap();
+        let num2 = caps.get(2).unwrap().as_str().parse::<u32>().unwrap();
+        sum += num1 * num2;
+    }
+    sum
 }
 
 pub fn part_2(input: &str) -> usize {
@@ -9,7 +23,7 @@ pub fn part_2(input: &str) -> usize {
 #[test]
 fn sample_input_part_1() {
     let input = include_str!("../input/sample_3.txt");
-    assert_eq!(part_1(input), 0)
+    assert_eq!(part_1(input), 161)
 }
 
 #[test]
