f3aa2f769f05802f2fea9f6ed2dba3d92ad5e19f
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-12-05 10:22:16 +0100

    Forbereder 6. desember

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -5,6 +5,7 @@ mod december_2;
 mod december_3;
 mod december_4;
 mod december_5;
+mod december_6;
 
 fn main() {
     let day = args().nth(1).unwrap();
@@ -32,6 +33,10 @@ fn main() {
         "5" => {
             println!("{}. desember, del 1: {:?}", day.as_str(), december_5::part_1(&input));
             println!("{}. desember, del 2: {:?}", day.as_str(), december_5::part_2(&input));
+        }
+        "6" => {
+            println!("{}. desember, del 1: {:?}", day.as_str(), december_6::part_1(&input));
+            println!("{}. desember, del 2: {:?}", day.as_str(), december_6::part_2(&input));
         }
         _ => ()
     }
