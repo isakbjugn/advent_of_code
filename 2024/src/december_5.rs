686502562c2030f7d412070ac1b7e33ab87c6d28
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-12-05 08:50:07 +0100

    Rydder 5. desember

--- 2024/src/december_5.rs
+++ 2024/src/december_5.rs
@@ -1,35 +1,13 @@
 use std::cmp::Ordering;
-use itertools::Itertools;
 
 pub fn part_1(input: &str) -> u32 {
-    let (rules_str, updates_str) = input.split_once("\n\n").expect("Wrong input format!");
-    let rules = rules_str
-        .lines()
-        .map(|line| line.split_once('|').expect("Rules not formatted as 1|2"))
-        .map(|(one, two)| (one.parse::<u32>().unwrap(), two.parse::<u32>().unwrap()))
-        .collect::<Vec<(u32, u32)>>();
+    let (rules, updates) = read_input(input);
 
-    let updates = updates_str
-        .lines()
-        .map(|line| line.split(",").map(|s| s.parse::<u32>().unwrap()).collect::<Vec<u32>>() )
-        .collect::<Vec<Vec<u32>>>();
-    
-    updates.into_iter().filter(|update| {
-        for (index, page) in update.iter().enumerate() {
-            if rules.iter().filter(|(one, _)| one == page)
-                .any(|(_, later_page)| {
-                    if let Some((later_index, _)) = update.iter().find_position(|&p| p == later_page ) {
-                        return if later_index < index { true } else { false }
-                    }
-                    false
-                }) {
-                return false
-            } else {
-                continue
-            }
-        }
-        true
-    })
+    updates
+        .into_iter()
+        .filter(|update| {
+            update.is_sorted_by(|a, b| sort_by_rules(a, b, &rules) != Ordering::Greater)
+        })
         .map(|update| {
             let middle_index = (update.len() - 1) / 2;
             *update.get(middle_index).expect("No middle element!")
@@ -38,6 +16,25 @@ pub fn part_1(input: &str) -> u32 {
 }
 
 pub fn part_2(input: &str) -> u32 {
+    let (rules, updates) = read_input(input);
+
+    updates
+        .into_iter()
+        .filter(|update| {
+            !update.is_sorted_by(|a, b| sort_by_rules(a, b, &rules) != Ordering::Greater)
+        })
+        .map(|mut update| {
+            update.sort_by(|a, b| sort_by_rules(a, b, &rules));
+            update
+        })
+        .map(|update| {
+            let middle_index = (update.len() - 1) / 2;
+            *update.get(middle_index).expect("No middle element!")
+        })
+        .sum()
+}
+
+fn read_input(input: &str) -> (Vec<(u32, u32)>, Vec<Vec<u32>>) {
     let (rules_str, updates_str) = input.split_once("\n\n").expect("Wrong input format!");
     let rules = rules_str
         .lines()
@@ -47,41 +44,25 @@ pub fn part_2(input: &str) -> u32 {
 
     let updates = updates_str
         .lines()
-        .map(|line| line.split(",").map(|s| s.parse::<u32>().unwrap()).collect::<Vec<u32>>() )
+        .map(|line| {
+            line.split(",")
+                .map(|s| s.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>()
+        })
         .collect::<Vec<Vec<u32>>>();
+    (rules, updates)
+}
 
-    updates.into_iter().filter(|update| {
-        update.iter().enumerate().any(|(index, page)| {
-            rules.iter()
-                .filter(|(one, _)| one == page)
-                .any(|(_, later_page)| {
-                    if let Some((later_index, _)) = update.iter().find_position(|&p| p == later_page ) {
-                        return if later_index < index { true } else { false }
-                    }
-                    false
-                })
-        })
-    })
-        .map(|update| {
-            let mut update = update.clone();
-            update.sort_by(|a, b| {
-                for (one, two) in rules.iter() {
-                    if one == a && two == b {
-                        return Ordering::Less
-                    }
-                    if one == b && two == a {
-                        return Ordering::Greater
-                    }
-                }
-                Ordering::Equal
-            });
-            update
-        })
-        .map(|update| {
-            let middle_index = (update.len() - 1) / 2;
-            *update.get(middle_index).expect("No middle element!")
-        })
-        .sum()
+fn sort_by_rules(a: &u32, b: &u32, rules: &[(u32, u32)]) -> Ordering {
+    for (one, two) in rules.iter() {
+        if one == a && two == b {
+            return Ordering::Less;
+        }
+        if one == b && two == a {
+            return Ordering::Greater;
+        }
+    }
+    Ordering::Equal
 }
 
 #[test]
