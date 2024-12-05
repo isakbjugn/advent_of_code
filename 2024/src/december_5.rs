31b853de37e3d2b8e828cce48c0d63b3bc706183
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-12-05 06:26:15 +0100

    Oppsett 5. desember

---
+++ 2024/src/december_5.rs
@@ -0,0 +1,31 @@
+pub fn part_1(input: &str) -> usize {
+    0
+}
+
+pub fn part_2(input: &str) -> usize {
+    0
+}
+
+#[test]
+fn sample_input_part_1() {
+    let input = include_str!("../input/sample_5.txt");
+    assert_eq!(part_1(input), 0)
+}
+
+#[test]
+fn sample_input_part_2() {
+    let input = include_str!("../input/sample_5.txt");
+    assert_eq!(part_2(input), 0)
+}
+
+#[test]
+fn input_part_1() {
+    let input = include_str!("../input/input_5.txt");
+    assert_eq!(part_1(input), 0)
+}
+
+#[test]
+fn input_part_2() {
+    let input = include_str!("../input/input_5.txt");
+    assert_eq!(part_2(input), 0)
+}
--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -4,6 +4,7 @@ mod december_1;
 mod december_2;
 mod december_3;
 mod december_4;
+mod december_5;
 
 fn main() {
     let day = args().nth(1).unwrap();
@@ -27,6 +28,10 @@ fn main() {
         "4" => {
             println!("{}. desember, del 1: {:?}", day.as_str(), december_4::part_1(&input));
             println!("{}. desember, del 2: {:?}", day.as_str(), december_4::part_2(&input));
+        }
+        "5" => {
+            println!("{}. desember, del 1: {:?}", day.as_str(), december_5::part_1(&input));
+            println!("{}. desember, del 2: {:?}", day.as_str(), december_5::part_2(&input));
         }
         _ => ()
     }
