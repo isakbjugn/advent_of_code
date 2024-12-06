82fe0c8b86c8dd6efe21ba0c3ed38a0de92e62bb
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-12-06 18:52:17 +0100

    Sjekker kun på #-felt i leads_to_loop

--- 2024/src/december_6.rs
+++ 2024/src/december_6.rs
@@ -13,7 +13,7 @@ pub fn part_1(input: &str) -> usize {
         visited.insert(position);
         match grid.next_value(&position, facing) {
             Some('.') | Some('^') => position = grid.next_position(&position, facing).expect("Not valid value on grid!"),
-            Some('#') => facing = facing.rotate_clockwise(),
+            Some('#') => facing = facing.clockwise(),
             _  => break
         }
     }
@@ -32,10 +32,10 @@ pub fn part_2(input: &str) -> usize {
         match grid.get(&next_cell) {
             Some('#') => facing = {
                 faced_obstacles.insert((next_cell, facing));
-                facing.rotate_clockwise()
+                facing.clockwise()
             },
             Some('.') | Some('^') => {
-                if leads_to_loop(position, facing, &grid, faced_obstacles.clone()) {
+                if leads_to_loop(position, facing.clockwise(), &grid, faced_obstacles.clone()) {
                     loop_positions.insert(next_cell);
                 }
                 position = next_cell;
@@ -44,23 +44,23 @@ pub fn part_2(input: &str) -> usize {
         }
     }
 
+    // loop_positions.into_iter().filter(|position| grid.get_value(position) != Some('#')).count()
     loop_positions.len()
 }
 
-fn leads_to_loop(position: Position, originally_facing: Direction, grid: &Grid, mut faced_obstacles: HashSet<(Position, Direction)>) -> bool {
-    let mut position = position;
-    let mut facing = originally_facing.rotate_clockwise();
-
+fn leads_to_loop(mut position: Position, mut facing: Direction, grid: &Grid, mut faced_obstacles: HashSet<(Position, Direction)>) -> bool {
+    let originally_facing = facing.counter_clockwise();
     faced_obstacles.insert((grid.next_position(&position, originally_facing).unwrap(), originally_facing));
 
     while let Some(next_cell) = grid.next_position(&position, facing) {
-        if faced_obstacles.contains(&(next_cell, facing)) {
-            return true;
-        }
         match grid.get(&next_cell) {
-            Some('#') => facing = {
-                faced_obstacles.insert((next_cell, facing));
-                facing.rotate_clockwise()
+            Some('#') => {
+                if faced_obstacles.contains(&(next_cell, facing)) {
+                    return true
+                } else {
+                    faced_obstacles.insert((next_cell, facing));
+                }
+                facing = facing.clockwise();
             },
             Some('.') | Some('^') => {
                 position = next_cell;
--- 2024/src/direction.rs
+++ 2024/src/direction.rs
@@ -7,32 +7,20 @@ pub enum Direction {
 }
 
 impl Direction {
-    pub fn opposite(&self) -> Direction {
-        match self {
-            Direction::North => Direction::South,
-            Direction::South => Direction::North,
-            Direction::East => Direction::West,
-            Direction::West => Direction::East,
-        }
-    }
-    pub fn perpendicular(&self) -> (Direction, Direction) {
-        match self {
-            Direction::North | Direction::South => (Direction::East, Direction::West),
-            Direction::East | Direction::West => (Direction::South, Direction::North),
-        }
-    }
-    pub fn perpendicular_option(&self) -> (Option<Direction>, Option<Direction>) {
-        match self {
-            Direction::North | Direction::South => (Some(Direction::East), Some(Direction::West)),
-            Direction::East | Direction::West => (Some(Direction::South), Some(Direction::North)),
-        }
-    }
-    pub fn rotate_clockwise(&self) -> Direction {
+    pub fn clockwise(&self) -> Direction {
         match self {
             Direction::North => Direction::East,
             Direction::East => Direction::South,
             Direction::South => Direction::West,
             Direction::West => Direction::North,
+        }
+    }
+    pub fn counter_clockwise(&self) -> Direction {
+        match self {
+            Direction::North => Direction::West,
+            Direction::West => Direction::South,
+            Direction::South => Direction::East,
+            Direction::East => Direction::North,
         }
     }
 }
