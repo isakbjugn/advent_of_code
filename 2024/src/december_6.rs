13c8c2dc09786137fe8e4017c7ce37e292507046
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-12-06 19:31:10 +0100

    Får ikke endre på felt som allerede er besøkt

--- 2024/src/december_6.rs
+++ 2024/src/december_6.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use itertools::Itertools;
 use crate::direction::Direction;
 use crate::grid::Grid;
 use crate::position::Position;
@@ -25,17 +26,19 @@ pub fn part_2(input: &str) -> usize {
     let mut position = grid.find('^').expect("Could not find starting position");
     let mut facing = Direction::North;
 
+    let mut visited = HashSet::<Position>::new();
     let mut faced_obstacles = HashSet::<(Position, Direction)>::new(); // obstacle and incoming direction
     let mut loop_positions = HashSet::<Position>::new();
 
     while let Some(next_cell) = grid.next_position(&position, facing) {
+        visited.insert(position);
         match grid.get(&next_cell) {
             Some('#') => facing = {
                 faced_obstacles.insert((next_cell, facing));
                 facing.clockwise()
             },
             Some('.') | Some('^') => {
-                if leads_to_loop(position, facing.clockwise(), &grid, faced_obstacles.clone()) {
+                if !visited.iter().contains(&next_cell) && leads_to_loop(position, facing.clockwise(), &grid, faced_obstacles.clone()) {
                     loop_positions.insert(next_cell);
                 }
                 position = next_cell;
@@ -50,16 +53,22 @@ pub fn part_2(input: &str) -> usize {
 
 fn leads_to_loop(mut position: Position, mut facing: Direction, grid: &Grid, mut faced_obstacles: HashSet<(Position, Direction)>) -> bool {
     let originally_facing = facing.counter_clockwise();
-    faced_obstacles.insert((grid.next_position(&position, originally_facing).unwrap(), originally_facing));
+    let new_obstacle = grid.next_position(&position, originally_facing).expect("Tried to create obstacle outside grid");
+    //grid.data[new_obstacle.y][new_obstacle.x] = '#';
+    if let Some(weird_obstacle) = faced_obstacles.iter().find(|&(position, _)| grid.get(position) == Some('.')) {
+        println!("Weird obstacle: {:?}", weird_obstacle);
+    }
+    
+    faced_obstacles.insert((new_obstacle, originally_facing));
 
     while let Some(next_cell) = grid.next_position(&position, facing) {
+        if faced_obstacles.contains(&(next_cell, facing)) {
+            // println!("Found already faced obstacle {:?} (facing {:?})", grid.get_value(&next_cell), facing);
+            return true
+        }
         match grid.get(&next_cell) {
             Some('#') => {
-                if faced_obstacles.contains(&(next_cell, facing)) {
-                    return true
-                } else {
-                    faced_obstacles.insert((next_cell, facing));
-                }
+                faced_obstacles.insert((next_cell, facing));
                 facing = facing.clockwise();
             },
             Some('.') | Some('^') => {
