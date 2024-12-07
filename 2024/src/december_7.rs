0a688cdbc1d15da15738957fcd9a40ce4022732e
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-12-07 10:06:44 +0100

    7. desember, del 2

--- 2024/src/december_7.rs
+++ 2024/src/december_7.rs
@@ -45,11 +45,55 @@ fn can_be_created(input: &(u64, Vec<u64>)) -> bool {
     false
 }
 
+pub fn part_2(input: &str) -> u64 {
+    input.lines()
+        .map(|line| line.split_once(": ").unwrap())
+        .map(|(first, second)| (
+            first.parse::<u64>().expect("Could not create ans value"),
+            second.split(" ")
+                .map(|string| string.parse::<u64>().expect("Could not create operand values"))
+                .collect::<Vec<u64>>())
+        )
+        .filter(can_be_created_2)
+        .map(|(ans, _)| ans)
+        .sum()
+}
 
+fn can_be_created_2(input: &(u64, Vec<u64>)) -> bool {
+    let (ans, operands) = input;
+    let operator_sequences = (1..=operands.len() - 1).map(|_| 1..=3).multi_cartesian_product()
+        .collect::<Vec<Vec<u64>>>();
 
+    // println!("Operator sequences: {:?}", operator_sequences);
+    for operator_sequence in operator_sequences {
+        let mut product_sum = *operands.first().unwrap();
+        // println!("Starting with {product_sum}");
+        for (index, operand) in operands.iter().skip(1).enumerate() {
+            let operator = match operator_sequence.get(index) {
+                Some(1) => "+",
+                Some(2) => "*",
+                Some(3) => "||",
+                _ => unreachable!()
+            };
+            // println!("Operation: {product_sum} {operator} {operand}");
+            match operator_sequence.get(index) {
+                Some(1) => product_sum += operand,
+                Some(2) => product_sum *= operand,
+                Some(3) => product_sum = concat(product_sum, *operand),
+                _ => unreachable!()
+            }
+            // println!("Result: {product_sum}");
+        }
+        // println!("Comparing {product_sum} and {ans}");
+        if product_sum == *ans {
+            return true
+        }
+    }
+    false
+}
 
-pub fn part_2(input: &str) -> usize {
-    0
+fn concat(a: u64, b: u64) -> u64 {
+    a * 10u64.pow(b.ilog10() + 1) + b
 }
 
 #[test]
@@ -61,7 +105,7 @@ fn sample_input_part_1() {
 #[test]
 fn sample_input_part_2() {
     let input = include_str!("../input/sample_7.txt");
-    assert_eq!(part_2(input), 0)
+    assert_eq!(part_2(input), 11387)
 }
 
 #[test]
@@ -73,5 +117,5 @@ fn input_part_1() {
 #[test]
 fn input_part_2() {
     let input = include_str!("../input/input_7.txt");
-    assert_eq!(part_2(input), 0)
+    assert_eq!(part_2(input), 581941094529163)
 }
