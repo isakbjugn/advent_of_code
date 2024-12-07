5dfb16a479c29965e36eb241db3e28edc2b7fd0f
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-12-07 10:38:12 +0100

    PermutationsWithReplacement som trait

--- 2024/src/december_7.rs
+++ 2024/src/december_7.rs
@@ -1,51 +1,20 @@
 use itertools::Itertools;
 
 pub fn part_1(input: &str) -> u64 {
-    input.lines()
-        .map(|line| line.split_once(": ").unwrap())
-        .map(|(first, second)| (
-            first.parse::<u64>().expect("Could not create ans value"),
-            second.split(" ")
-                .map(|string| string.parse::<u64>().expect("Could not create operand values"))
-                .collect::<Vec<u64>>())
-        )
-        .filter(can_be_created)
+    to_ans_and_operands(input)
+        .filter(|(ans, operands)| can_be_created(ans, operands, vec!['+', '*']))
         .map(|(ans, _)| ans)
         .sum()
-}
-
-fn can_be_created(input: &(u64, Vec<u64>)) -> bool {
-    let (ans, operands) = input;
-    let operator_sequences = (1..=operands.len() - 1).map(|_| 1..=2).multi_cartesian_product()
-        .collect::<Vec<Vec<u64>>>();
-
-    // println!("Operator sequences: {:?}", operator_sequences);
-    for operator_sequence in operator_sequences {
-        let mut product_sum = *operands.first().unwrap();
-        // println!("Starting with {product_sum}");
-        for (index, operand) in operands.iter().skip(1).enumerate() {
-            let operator = match operator_sequence.get(index) {
-                Some(1) => '+',
-                Some(2) => '*',
-                _ => unreachable!()
-            };
-            // println!("Operation: {product_sum} {operator} {operand}");
-            match operator_sequence.get(index) {
-                Some(1) => product_sum += operand,
-                Some(2) => product_sum *= operand,
-                _ => unreachable!()
-            }
-            // println!("Result: {product_sum}");
-        }
-        // println!("Comparing {product_sum} and {ans}");
-        if product_sum == *ans {
-            return true
-        }
-    }
-    false
 }
 
 pub fn part_2(input: &str) -> u64 {
+    to_ans_and_operands(input)
+        .filter(|(ans, operands)| can_be_created(ans, operands, vec!['+', '*', '|']))
+        .map(|(ans, _)| ans)
+        .sum()
+}
+
+fn to_ans_and_operands(input: & str) -> impl Iterator<Item = (u64, Vec<u64>)> + use<'_> {
     input.lines()
         .map(|line| line.split_once(": ").unwrap())
         .map(|(first, second)| (
@@ -54,37 +23,21 @@ pub fn part_2(input: &str) -> u64 {
                 .map(|string| string.parse::<u64>().expect("Could not create operand values"))
                 .collect::<Vec<u64>>())
         )
-        .filter(can_be_created_2)
-        .map(|(ans, _)| ans)
-        .sum()
 }
 
-fn can_be_created_2(input: &(u64, Vec<u64>)) -> bool {
-    let (ans, operands) = input;
-    let operator_sequences = (1..=operands.len() - 1).map(|_| 1..=3).multi_cartesian_product()
-        .collect::<Vec<Vec<u64>>>();
+fn can_be_created(ans: &u64, operands: &[u64], basis: Vec<char>) -> bool {
+    let operator_sequences = basis.permutations_with_replacement(operands.len() - 1);
 
-    // println!("Operator sequences: {:?}", operator_sequences);
     for operator_sequence in operator_sequences {
         let mut product_sum = *operands.first().unwrap();
-        // println!("Starting with {product_sum}");
         for (index, operand) in operands.iter().skip(1).enumerate() {
-            let operator = match operator_sequence.get(index) {
-                Some(1) => "+",
-                Some(2) => "*",
-                Some(3) => "||",
-                _ => unreachable!()
-            };
-            // println!("Operation: {product_sum} {operator} {operand}");
             match operator_sequence.get(index) {
-                Some(1) => product_sum += operand,
-                Some(2) => product_sum *= operand,
-                Some(3) => product_sum = concat(product_sum, *operand),
+                Some('+') => product_sum += operand,
+                Some('*') => product_sum *= operand,
+                Some('|') => product_sum = concat(product_sum, *operand),
                 _ => unreachable!()
             }
-            // println!("Result: {product_sum}");
         }
-        // println!("Comparing {product_sum} and {ans}");
         if product_sum == *ans {
             return true
         }
@@ -94,6 +47,25 @@ fn can_be_created_2(input: &(u64, Vec<u64>)) -> bool {
 
 fn concat(a: u64, b: u64) -> u64 {
     a * 10u64.pow(b.ilog10() + 1) + b
+}
+
+trait PermutationsWithReplacement {
+    fn permutations_with_replacement(&self, k: usize) -> Vec<Self> where Self: Sized;
+}
+
+impl PermutationsWithReplacement for Vec<char> {
+    fn permutations_with_replacement(&self, k: usize) -> Vec<Self> {
+        (0..=k - 1).map(|_| 0..=self.len() - 1)
+            .multi_cartesian_product()
+            .map(|permutation|
+                permutation
+                    .into_iter()
+                    .map(|index| self.get(index).expect("Could not access basis"))
+                    .cloned()
+                    .collect()
+            )
+            .collect::<Vec<Vec<char>>>()
+    }
 }
 
 #[test]
