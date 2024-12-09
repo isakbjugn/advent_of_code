960c578d467ef35c2a9a737ac8d06119439533c3
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-12-09 06:27:42 +0100

    Oppset 9. desember

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -12,6 +12,7 @@ mod december_5;
 mod december_6;
 mod december_7;
 mod december_8;
+mod december_9;
 
 fn main() {
     let day = args().nth(1).unwrap();
@@ -51,6 +52,10 @@ fn main() {
         "8" => {
             println!("{}. desember, del 1: {:?}", day.as_str(), december_8::part_1(&input));
             println!("{}. desember, del 2: {:?}", day.as_str(), december_8::part_2(&input));
+        }
+        "9" => {
+            println!("{}. desember, del 1: {:?}", day.as_str(), december_9::part_1(&input));
+            println!("{}. desember, del 2: {:?}", day.as_str(), december_9::part_2(&input));
         }
         _ => ()
     }
