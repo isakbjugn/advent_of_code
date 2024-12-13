fe1af682e1d437011500a19c33c7f5db7a41e92e
Isak Kyrre Lichtwarck Bjugn <42857774+isakbjugn@users.noreply.github.com>
2024-12-13 16:32:13 +0100

    Raskere 6. desember med bedre logikk og datastrukturer (#2)
    * Informert tilnærming, feil svar
    
    * Sjekker kun på #-felt i leads_to_loop
    
    * Får ikke endre på felt som allerede er besøkt
    
    * Lager tilnærming på nytt fra fungerende des 6-implementasjon
    
    * Gir ny grid til leads_to_loop. Får riktig svar.
    
    * Får til parallelisering. 20 ms
    
    * Vec<Vec<bool>> for visited, kutter litt kjøretid
    
    * rustc_hash:FxHashSet, unngår kloning av Grid
    
    * 13. desember, del 2

--- 2024/src/december_6.rs
+++ 2024/src/december_6.rs
@@ -1,69 +1,86 @@
-use std::collections::HashSet;
-use itertools::Itertools;
 use rayon::prelude::*;
 use crate::direction::Direction;
 use crate::grid::Grid;
 use crate::position::Position;
+use rustc_hash::FxHashSet;
 
 pub fn part_1(input: &str) -> usize {
-    find_path(input).len()
-}
-
-fn find_path(input: &str) -> HashSet<Position> {
     let grid = Grid::from_str(input).expect("Unable to create Grid!");
     let mut position = grid.find('^').expect("Could not find starting position");
-    let mut visited = HashSet::<Position>::new();
+    let mut visited = FxHashSet::<Position>::default();
     let mut facing = Direction::North;
 
     loop {
         visited.insert(position);
         match grid.next_value(&position, facing) {
             Some('.') | Some('^') => position = grid.next_position(&position, facing).expect("Not valid value on grid!"),
-            Some('#') => facing = facing.rotate_clockwise(),
+            Some('#') => facing = facing.clockwise(),
             _  => break
         }
     }
-    visited
+    visited.len()
 }
 
 pub fn part_2(input: &str) -> usize {
-    let visited = find_path(input);
-
     let grid = Grid::from_str(input).expect("Unable to create Grid!");
-    let starting_position = grid.find('^').expect("Could not find starting position");
-    let starting_direction = Direction::North;
+    let mut position = grid.find('^').expect("Could not find starting position");
+    let mut not_visited: Vec<Vec<bool>> = vec![vec![true; grid.width()]; grid.height()];
+    let mut facing = Direction::North;
+    let mut faced_obstacles: FxHashSet<(Position, Direction)> = FxHashSet::default();
     
-    visited.into_par_iter()
-        .filter(|position| {
-            let grid = {
-                let mut grid = grid.clone();
-                grid.set(*position, '#');
-                grid
-            };
-            
-            let mut faced_obstacles: HashSet<(Position, Direction)> = HashSet::new();
-            
-            let mut position = starting_position;
-            let mut facing = starting_direction;
+    let mut should_check: Vec<(Position, Direction, FxHashSet<(Position, Direction)>)> = Vec::new();
+
+    while let Some(next_position) = grid.next_position(&position, facing) {
+        not_visited[position.y][position.x] = false;
+        match grid.get(&next_position) {
+            Some('.') | Some('^') => {
+                let next_position = grid.next_position(&position, facing).expect("Not valid value on grid!");
+                if not_visited[next_position.y][next_position.x] {
+                    should_check.push((position, facing, faced_obstacles.clone()))
+                }
+                position = next_position;
+            },
+            Some('#') => {
+                let obstacle = grid.next_position(&position, facing).expect("Not valid value on grid!");
+                faced_obstacles.insert((obstacle, facing));
+                facing = facing.clockwise()
+            },
+            _  => unreachable!()
+        }
+    }
     
-            while let Some(next_cell) = grid.next_position(&position, facing) {
-                if faced_obstacles.contains(&(next_cell, facing)) {
-                    return true
-                }
-                
-                match grid.get(&next_cell) {
-                    Some('#') => facing = {
-                        faced_obstacles.insert((next_cell, facing));
-                        facing.rotate_clockwise()
-                    },
-                    Some('.') | Some('^') => {
-                        position = next_cell;
-                    }
-                    _ => { unreachable!() }
-                }
+    should_check.into_par_iter()
+        .filter(|args| {
+            let next_position = grid.next_position(&args.0, args.1).expect("Should be valid next position");
+            leads_to_loop(args.0, args.1, next_position, &grid, args.2.clone())
+        })
+        .count()
+}
+
+fn leads_to_loop(mut position: Position, mut facing: Direction, obstacle: Position, grid: &Grid, mut faced_obstacles: FxHashSet<(Position, Direction)>) -> bool {
+    while let Some(next_cell) = grid.next_position(&position, facing) {
+        if faced_obstacles.contains(&(next_cell, facing)) {
+            return true
+        }
+        
+        if next_cell == obstacle {
+            faced_obstacles.insert((next_cell, facing));
+            facing = facing.clockwise();
+            continue
+        }
+
+        match grid.get(&next_cell) {
+            Some('#') => {
+                faced_obstacles.insert((next_cell, facing));
+                facing = facing.clockwise();
+            },
+            Some('.') | Some('^') => {
+                position = next_cell;
             }
-            false
-        }).count()
+            _ => { unreachable!() }
+        }
+    }
+    false
 }
 
 #[test]
