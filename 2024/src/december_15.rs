d91cb834733703912c27616c5169c9b4befc6b90
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-12-15 16:50:26 +0100

    15. desember: Sorterer for alle

--- 2024/src/december_15.rs
+++ 2024/src/december_15.rs
@@ -71,24 +71,15 @@ pub fn part_2(input: &str) -> u32 {
         };
 
         if let Path::Open(crates_to_move) = find_crates_to_move(&warehouse, robot_position, direction) {
-            if (1..crates_to_move.len()).any(|i| crates_to_move[i..].contains(&crates_to_move[i - 1])) {
-                let sorted: Vec<Position> = crates_to_move.clone().into_iter()
-                    .unique()
-                    .sorted_by(|a, b| sort_by_direction(a, b, direction))
-                    .collect();
-                for tile in sorted.into_iter() {
-                    let value = warehouse.get(&tile).expect("No valid current value");
-                    let next_tile = warehouse.next_position(&tile, direction).expect("No valid next tile");
-                    warehouse.set(next_tile, value);
-                    warehouse.set(tile, '.')
-                }
-            } else {
-                for tile in crates_to_move.into_iter().rev() {
-                    let value = warehouse.get(&tile).expect("No valid current value");
-                    let next_tile = warehouse.next_position(&tile, direction).expect("No valid next tile");
-                    warehouse.set(next_tile, value);
-                    warehouse.set(tile, '.')
-                }
+            let sorted: Vec<Position> = crates_to_move.clone().into_iter()
+                .unique()
+                .sorted_by(|a, b| sort_by_direction(a, b, direction))
+                .collect();
+            for tile in sorted.into_iter() {
+                let value = warehouse.get(&tile).expect("No valid current value");
+                let next_tile = warehouse.next_position(&tile, direction).expect("No valid next tile");
+                warehouse.set(next_tile, value);
+                warehouse.set(tile, '.')
             }
         }
     }
