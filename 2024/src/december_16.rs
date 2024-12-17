a402d07912bbdcfa810937f9b21f9ec5e7a41011
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-12-17 07:18:31 +0100

    16. desember, del 2

--- 2024/src/december_16.rs
+++ 2024/src/december_16.rs
@@ -1,6 +1,5 @@
 use std::cmp::Ordering;
-use std::collections::{BinaryHeap, HashMap};
-use itertools::Itertools;
+use std::collections::{BinaryHeap, HashMap, HashSet};
 use crate::direction::Direction;
 use crate::grid::Grid;
 use crate::position::Position;
@@ -11,14 +10,19 @@ pub fn part_1(input: &str) -> usize {
 }
 
 pub fn part_2(input: &str) -> usize {
-    0
+    let maze = Grid::from_str(input).expect("Could not create maze");
+    let optimal_path_tiles = maze.dijkstra_positions();
+
+    let mut optimal_path = maze.clone();
+    optimal_path_tiles.iter().for_each(|&position| optimal_path.set(position, 'O'));
+    println!("{}", optimal_path);
+    optimal_path_tiles.len()
 }
 
 #[derive(PartialEq, Eq, Clone, Hash, Debug)]
 struct Vertex {
     position: Position,
     direction: Direction,
-    straight_path: usize,
 }
 
 // Define a wrapper type for the heap
@@ -29,6 +33,14 @@ struct AStarState {
     vertex: Vertex,
 }
 
+// Define a wrapper type for the heap
+#[derive(Clone, Eq, PartialEq)]
+struct DijkstraState {
+    cost: usize,
+    vertex: Vertex,
+    path: Vec<Position>,
+}
+
 // Implement `Ord` for `State`. We want to order states by cost in reverse order since `BinaryHeap` is a max heap.
 impl Ord for AStarState {
     fn cmp(&self, other: &Self) -> Ordering {
@@ -37,6 +49,19 @@ impl Ord for AStarState {
 }
 
 impl PartialOrd for AStarState {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+// Implement `Ord` for `State`. We want to order states by cost in reverse order since `BinaryHeap` is a max heap.
+impl Ord for DijkstraState {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.cost.cmp(&self.cost)
+    }
+}
+
+impl PartialOrd for DijkstraState {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
@@ -64,10 +89,8 @@ impl Grid {
             vertex: Vertex {
                 position: start_pos,
                 direction: Direction::East,
-                straight_path: 0
             }
         });
-
 
         while let Some(AStarState { cost, vertex, .. }) = heap.pop() {
             if self.is_goal_state(&vertex) {
@@ -88,7 +111,6 @@ impl Grid {
                 let new_vertex = Vertex {
                     position: next_position,
                     direction: next_direction,
-                    straight_path: count_straight_path(vertex.straight_path, vertex.direction, next_direction)
                 };
 
                 if !distances.contains_key(&new_vertex) || new_cost < distances[&new_vertex] {
@@ -103,17 +125,66 @@ impl Grid {
 
         None
     }
+    fn dijkstra_positions(&self) -> HashSet<Position> {
+        let mut heap = BinaryHeap::new();
+        let mut distances = HashMap::new();
+        let mut optimal_cost = None;
+        let mut result_positions: HashSet<Position> = HashSet::new();
+
+        let start_pos = self.find('S').expect("Must be a start position");
+        heap.push(DijkstraState {
+            cost: 0,
+            vertex: Vertex {
+                position: start_pos,
+                direction: Direction::East,
+            },
+            path: vec![start_pos]
+        });
+
+        while let Some(DijkstraState { cost, vertex, path, .. }) = heap.pop() {
+            if self.is_goal_state(&vertex) {
+                // Update the optimal cost on the first goal state encounter
+                if optimal_cost.is_none() {
+                    optimal_cost = Some(cost);
+                }
+                // Add all positions in this path to the result set
+                if cost == optimal_cost.unwrap() {
+                    path.iter().for_each(|&position| { result_positions.insert(position); });
+                }
+                continue;
+            }
+
+            if let Some(&d) = distances.get(&vertex) {
+                if cost > d {
+                    continue;
+                }
+            }
+
+            distances.insert(vertex.clone(), cost);
+
+            for (next_position, next_direction, added_cost) in self.next_vertices(vertex.position, vertex.direction) {
+                let new_cost = cost + added_cost;
+                let new_vertex = Vertex {
+                    position: next_position,
+                    direction: next_direction,
+                };
+
+                if !distances.contains_key(&new_vertex) || new_cost < distances[&new_vertex] {
+                    heap.push(DijkstraState {
+                        cost: new_cost,
+                        vertex: new_vertex,
+                        path: [next_position].iter().chain(path.iter()).cloned().collect(),
+                    });
+                }
+            }
+        }
+
+        result_positions
+    }
     fn manhattan_distance(&self, pos: Position) -> usize {
         let dx = self.width() - 1 - pos.x;
         let dy = self.height() - 1 - pos.y;
         dx + dy
-    }
-}
-
-fn count_straight_path(straight_path: usize, previous_direction: Direction, new_direction: Direction) -> usize {
-    match previous_direction == new_direction {
-        true => straight_path + 1,
-        false => 1,
     }
 }
 
@@ -132,13 +203,13 @@ fn sample_input_part_1_2() {
 #[test]
 fn sample_input_part_2_1() {
     let input = include_str!("../input/sample_16_1.txt");
-    assert_eq!(part_2(input), 0)
+    assert_eq!(part_2(input), 45)
 }
 
 #[test]
 fn sample_input_part_2_2() {
     let input = include_str!("../input/sample_16_2.txt");
-    assert_eq!(part_2(input), 0)
+    assert_eq!(part_2(input), 64)
 }
 
 #[test]
@@ -150,5 +221,5 @@ fn input_part_1() {
 #[test]
 fn input_part_2() {
     let input = include_str!("../input/input_16.txt");
-    assert_eq!(part_2(input), 0)
+    assert_eq!(part_2(input), 679)
 }
