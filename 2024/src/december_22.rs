53deda4e360e737770a37734a6b6853f3fc1909e
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-12-22 09:59:21 +0100

    22. desember, del 1

--- 2024/src/december_22.rs
+++ 2024/src/december_22.rs
@@ -1,6 +1,30 @@
+use rayon::prelude::*;
 
-pub fn part_1(input: &str) -> usize {
-    0
+pub fn part_1(input: &str) -> u64 {
+    input.par_lines()
+        .map(|line| line.parse::<u64>().expect("Secret number must parse to int"))
+        .map(|secret_number| {
+            let mut number = secret_number;
+            for _ in 0..2000 {
+                number = generate(number);
+            }
+            // println!("{}: {}", secret_number, number);
+            number
+        })
+        .sum()
+}
+
+fn generate(number: u64) -> u64 {
+    let first_mixer = number * 64;
+    let number = mix_and_prune(number, first_mixer);
+    let second_mixer = number / 32;
+    let number = mix_and_prune(number, second_mixer);
+    let third_mixer = number * 2048;
+    mix_and_prune(number, third_mixer)
+}
+
+fn mix_and_prune(number: u64, mixer: u64) -> u64 {
+    (number ^ mixer) % 16777216
 }
 
 pub fn part_2(input: &str) -> usize {
@@ -10,7 +34,7 @@ pub fn part_2(input: &str) -> usize {
 #[test]
 fn sample_input_part_1() {
     let input = include_str!("../input/sample_22.txt");
-    assert_eq!(part_1(input), 0)
+    assert_eq!(part_1(input), 37327623)
 }
 
 #[test]
@@ -22,7 +46,7 @@ fn sample_input_part_2() {
 #[test]
 fn input_part_1() {
     let input = include_str!("../input/input_22.txt");
-    assert_eq!(part_1(input), 0)
+    assert_eq!(part_1(input), 17163502021)
 }
 
 #[test]
