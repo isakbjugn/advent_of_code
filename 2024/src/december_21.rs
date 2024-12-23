99b124ffe5e63bc34615ec3835e36b02a38fab7e
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-12-23 11:15:33 +0100

    Rydder i 21. desember

--- 2024/src/december_21.rs
+++ 2024/src/december_21.rs
@@ -3,27 +3,21 @@ use itertools::Itertools;
 use crate::with::{With};
 
 pub fn part_1(input: &str) -> u64 {
-    let mut memo: HashMap<(char, char, u8), u64> = HashMap::new();
-    input.lines()
-        .map(|line| line.chars().collect::<Vec<char>>())
-        .map(|code| -> u64 {
-            let combination_length: u64 = ['A'].with(&code).into_iter()
-                .tuple_windows()
-                .map(|(from, to)| find_combination_length(from, to, 3, 3, &mut memo))
-                .sum();
-            combination_length * code_to_number(&code).unwrap() as u64
-        })
-        .sum()
+    sum_complexities(input, 2)
 }
 
 pub fn part_2(input: &str) -> u64 {
+    sum_complexities(input, 25)
+}
+
+fn sum_complexities(input: &str, robots: u8) -> u64 {
     let mut memo: HashMap<(char, char, u8), u64> = HashMap::new();
     input.lines()
         .map(|line| line.chars().collect::<Vec<char>>())
         .map(|code| -> u64 {
             let combination_length: u64 = ['A'].with(&code).into_iter()
                 .tuple_windows()
-                .map(|(from, to)| find_combination_length(from, to, 26, 26, &mut memo))
+                .map(|(from, to)| find_combination_length(from, to, robots, robots, &mut memo))
                 .sum();
             combination_length * code_to_number(&code).unwrap() as u64
         })
@@ -35,71 +29,51 @@ fn find_combination_length(from: char, to: char, robots: u8, max_robots: u8, mem
         return result
     }
     let result = match robots {
-        0 => 1,
-        r if r == max_robots => {
-            match move_on_num_pad(from, to) {
-                (button_sequence, false) => {
-                    ['A'].with(&button_sequence).with(&['A']).into_iter()
-                        .tuple_windows()
-                        .map(|(from, to)| find_combination_length(from, to, robots - 1, max_robots, memo))
-                        .sum()
-                }
-                (button_sequence, true) => {
-                    let k = button_sequence.len();
-                    button_sequence.into_iter().permutations(k)
-                        .map(|permuted_sequence| {
-                            ['A'].with(&permuted_sequence).with(&['A']).into_iter()
-                                .tuple_windows()
-                                .map(|(from, to)| find_combination_length(from, to, robots - 1, max_robots, memo))
-                                .sum()
-                        })
-                        .min().expect("Should be a minimum")
-                }
-            }
+        0 => {
+            ['A'].with(&move_on_d_pad(from, to).0).len() as u64
         }
-        _ => {
-            match move_on_d_pad(from, to) {
-                (button_sequence, false) => {
-                    ['A'].with(&button_sequence).with(&['A']).into_iter()
+        r => {
+            let get_moves = if r == max_robots { move_on_num_pad } else { move_on_d_pad };
+            moves_with_all_variants(from, to, get_moves).into_iter()
+                .map(|sequence| {
+                    sequence.into_iter()
                         .tuple_windows()
                         .map(|(from, to)| find_combination_length(from, to, robots - 1, max_robots, memo))
                         .sum()
-                }
-                (button_sequence, true) => {
-                    let k = button_sequence.len();
-                    button_sequence.into_iter().permutations(k)
-                        .map(|permuted_sequence| {
-                            ['A'].with(&permuted_sequence).with(&['A']).into_iter()
-                                .tuple_windows()
-                                .map(|(from, to)| find_combination_length(from, to, robots - 1, max_robots, memo))
-                                .sum()
-                        })
-                        .min().expect("Should be a minimum")
-                }
-            }
+                })
+                .min().expect("Should be a minimum")
         }
     };
     memo.insert((from, to, robots), result);
     result
 }
 
-fn code_to_number(code: &Vec<char>) -> Option<usize> {
+fn code_to_number(code: &[char]) -> Option<usize> {
     let numeric_string: String = code
-        .into_iter()
+        .iter()
         .filter(|c| c.is_ascii_digit())
         .collect();
 
-    // Parse the resulting string into a u64
     numeric_string.parse::<usize>().ok()
+}
+
+fn moves_with_all_variants(from: char, to: char, get_moves: fn(char, char) -> (Vec<char>, bool)) -> Vec<Vec<char>> {
+    match get_moves(from, to) {
+        (sequence, false) => vec![['A'].with(&sequence).with(&['A'])],
+        (sequence, true) => {
+            let k = sequence.len();
+            sequence.into_iter().permutations(k).map(|permuted_sequence| ['A'].with(&permuted_sequence).with(&['A'])).collect()
+        }
+    }
 }
 
 fn move_on_num_pad(from: char, to: char) -> (Vec<char>, bool) {
     match (from, to) {
         ('A', '0') => (vec!['<'], true),
-        ('A', '1') => (vec!['^', '<', '<'], false), // unngå ulovlig posisjon true),
+        ('A', '1') => (vec!['^', '<', '<'], false),
         ('A', '3') => (vec!['^'], true),
-        ('A', '4') => (vec!['^', '^', '<', '<'], false), // unngå ulovlig posisjon true),
-        ('A', '7') => (vec!['^', '^', '^', '<', '<'], false), // unngå ulovlig posisjon true),
+        ('A', '4') => (vec!['^', '^', '<', '<'], false),
+        ('A', '7') => (vec!['^', '^', '^', '<', '<'], false),
         ('A', '8') => (vec!['<', '^', '^', '^'], true),
         ('A', '9') => (vec!['^', '^', '^'], true),
 
@@ -189,9 +163,7 @@ fn sample_input_part_2() {
 #[test]
 fn input_part_1() {
     let input = include_str!("../input/input_21.txt");
-    let result = part_1(input);
-    println!("Result: {}", result);
-    assert_eq!(result, 270084)
+    assert_eq!(part_1(input), 270084)
 }
 
 #[test]
