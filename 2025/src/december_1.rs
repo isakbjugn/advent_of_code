13c854a4528ff4f9c1ee87f0cedd6e9fac20b12b
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-12-01 06:58:12 +0100

    1. desember, del 2

--- 2025/src/december_1.rs
+++ 2025/src/december_1.rs
@@ -17,12 +17,63 @@ pub fn part_1(input: &str) -> u32 {
     zeros
 }
 
-pub fn part_2(input: &str) -> u32 {
-    0
+pub fn part_2(input: &str) -> i32 {
+    let mut dial = 50;
+    let mut zeros = 0;
+    for line in input.lines() {
+        let (direction, value) = line.split_at(1);
+        let value: i32 = value.parse().unwrap();
+        dial = match direction {
+            "L" => {
+                let new_value = dial - value;
+                if new_value == 0 {
+                    zeros += 1;
+                }
+                else if new_value < 0 && dial == 0 {
+                    let clicks = new_value / 100;
+                    zeros -= clicks;
+                }
+                else if new_value < 0 {
+                    let clicks = -1 + new_value / 100;
+                    zeros -= clicks;
+                }
+                new_value % 100
+            },
+            "R" => {
+                let new_value = dial + value;
+                let clicks = new_value / 100;
+                zeros += clicks;
+                new_value % 100
+            },
+            &_ => unreachable!(),
+        };
+        if dial < 0 {
+            dial += 100;
+        }
+    }
+    zeros
 }
 
 #[test]
 fn sample_input_part_1() {
     let input = include_str!("../input/sample_1.txt");
     assert_eq!(part_1(input), 3)
+}
+
+#[test]
+fn input_part_1() {
+    let input = include_str!("../input/input_1.txt");
+    assert_eq!(part_1(input), 1052)
+}
+
+#[test]
+fn sample_input_part_2() {
+    let input = include_str!("../input/sample_1.txt");
+    assert_eq!(part_2(input), 6)
+}
+
+#[test]
+fn input_part_2() {
+    let input = include_str!("../input/input_1.txt");
+    assert_eq!(part_2(input), 6295)
 }
