0b1581148f5f01c1ce1c32748097222ef8bf979a
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-12-02 06:30:55 +0100

    2. desember, del 2

--- 2025/src/december_2.rs
+++ 2025/src/december_2.rs
@@ -27,8 +27,34 @@ fn is_repeated(number: u64) -> bool {
     }
 }
 
-pub fn part_2(input: &str) -> u32 {
-    0
+pub fn part_2(input: &str) -> u64 {
+    let ranges = input.split(',');
+    let mut sum = 0;
+    for range in ranges {
+        let (first, last) = range.split_once('-').expect("Burde være bindestrek som skilletegn");
+        let first = first.parse::<u64>().expect("Må være heltall");
+        let last = last.parse::<u64>().expect("Må være heltall");
+        for number in first..=last {
+            if is_repeated_any_number_of_times(number) {
+                sum += number;
+            }
+        }
+    }
+    sum
+}
+
+fn is_repeated_any_number_of_times(number: u64) -> bool {
+    for char in 1..=number.to_string().len()/2 {
+        let substring = &number.to_string()[0..char];
+        let mut repeated = String::new();
+        while repeated.len() < number.to_string().len() {
+            repeated.push_str(substring);
+        }
+        if repeated == number.to_string() {
+            return true;
+        }
+    }
+    false
 }
 
 #[test]
@@ -46,11 +72,11 @@ fn input_part_1() {
 #[test]
 fn sample_input_part_2() {
     let input = include_str!("../input/sample_2.txt");
-    //assert_eq!(part_2(input), 0)
+    assert_eq!(part_2(input), 4174379265)
 }
 
 #[test]
 fn input_part_2() {
     let input = include_str!("../input/input_2.txt");
-    //assert_eq!(part_2(input), 0)
+    assert_eq!(part_2(input), 85513235135)
 }
