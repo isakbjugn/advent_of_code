1c7140f7c0fbd5a0534f589a8212edd476f09ecd
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-12-02 06:19:03 +0100

    2. desember, del 1

--- 2025/src/december_2.rs
+++ 2025/src/december_2.rs
@@ -1,5 +1,30 @@
-pub fn part_1(input: &str) -> u32 {
-    0
+pub fn part_1(input: &str) -> u64 {
+    let ranges = input.split(',');
+    let mut sum = 0;
+    for range in ranges {
+        let (first, last) = range.split_once('-').expect("Burde være bindestrek som skilletegn");
+        let first = first.parse::<u64>().expect("Må være heltall");
+        let last = last.parse::<u64>().expect("Må være heltall");
+        for number in first..=last {
+            if is_repeated(number) {
+                sum += number;
+            }
+        }
+    }
+    sum
+}
+
+fn is_repeated(number: u64) -> bool {
+    match number.to_string().len() % 2 {
+        0 => {
+            let s = number.to_string();
+            let mid = s.len() / 2;
+            let first_half = &s[..mid];
+            let second_half = &s[mid..];
+            first_half == second_half
+        }
+        _ => false,
+    }
 }
 
 pub fn part_2(input: &str) -> u32 {
@@ -15,7 +40,7 @@ fn sample_input_part_1() {
 #[test]
 fn input_part_1() {
     let input = include_str!("../input/input_2.txt");
-    //assert_eq!(part_1(input), 0)
+    assert_eq!(part_1(input), 64215794229)
 }
 
 #[test]
