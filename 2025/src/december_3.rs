ed95e0b60058a3514e1cea452a7f212bca2c8270
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-12-03 06:04:43 +0100

    Input dag 3

--- 2025/src/december_3.rs
+++ 2025/src/december_3.rs
@@ -8,24 +8,24 @@ pub fn part_2(input: &str) -> u64 {
 
 #[test]
 fn sample_input_part_1() {
-    //let input = include_str!("../input/sample_3.txt");
+    let input = include_str!("../input/sample_3.txt");
     //assert_eq!(part_1(input), 0)
 }
 
 #[test]
 fn input_part_1() {
-    //let input = include_str!("../input/input_3.txt");
+    let input = include_str!("../input/input_3.txt");
     //assert_eq!(part_1(input), 0)
 }
 
 #[test]
 fn sample_input_part_2() {
-    //let input = include_str!("../input/sample_3.txt");
+    let input = include_str!("../input/sample_3.txt");
     //assert_eq!(part_2(input), 0)
 }
 
 #[test]
 fn input_part_2() {
-    //let input = include_str!("../input/input_3.txt");
+    let input = include_str!("../input/input_3.txt");
     //assert_eq!(part_2(input), 0)
 }
