0ed16379097c9dcb5bb0dae156504072e9cbb2eb
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-12-03 23:10:17 +0100

    Refaktorerer 3. desember med rekursjon

--- 2025/src/december_3.rs
+++ 2025/src/december_3.rs
@@ -1,59 +1,31 @@
-pub fn part_1(input: &str) -> u32 {
+pub fn part_1(input: &str) -> u64 {
     input
         .lines()
-        .map(get_max_joltage_with_two_batteries)
+        .map(|line| line.chars().filter_map(|c| c.to_digit(10).map(u64::from)).collect())
+        .map(|bank| get_max_joltage(bank, 2))
         .sum()
-}
-
-fn get_max_joltage_with_two_batteries(bank: &str) -> u32 {
-    let first = 0;
-    for first_digit in (1..=9).rev() {
-        // find first occurrence of digit in bank
-        if let Some(pos) = bank.find(char::from_digit(first_digit, 10).unwrap()) {
-            if let Some(second_digit) = bank[pos + 1..]
-                .chars()
-                .filter_map(|c| c.to_digit(10))
-                .max() {
-                return first_digit * 10 + second_digit;
-            }
-        }
-    }
-    first
 }
 
 pub fn part_2(input: &str) -> u64 {
     input
         .lines()
-        .map(get_largest_twelve_digit_number_from_string)
+        .map(|line| line.chars().filter_map(|c| c.to_digit(10).map(u64::from)).collect())
+        .map(|bank| get_max_joltage(bank, 12))
         .sum()
 }
 
-fn get_largest_twelve_digit_number_from_string(bank: &str) -> u64 {
-    let digits = bank.chars().filter_map(|c| c.to_digit(10)).collect::<Vec<u32>>();
-    let mut selection = digits[0..12].to_vec();
-    for position in 12..digits.len() {
-        // if removing the smallest digit and adding the new digit results in a larger number, do it
-        let next_digit = digits[position];
-        let mut max = selection.iter().fold(0u64, |acc, &d| acc * 10 + d as u64);
-        let mut remove_position = None;
-        for new_position in 0..12 {
-            let mut temp_selection = selection.clone();
-            temp_selection.remove(new_position);
-            temp_selection.push(next_digit);
-            let sum = temp_selection.iter().fold(0u64, |acc, &d| acc * 10 + d as u64);
-            if sum > max {
-                remove_position = Some(new_position);
-                max = sum;
-            }
-        }
-        if let Some(pos) = remove_position {
-            selection.remove(pos);
-            selection.push(next_digit);
+fn get_max_joltage(bank: Vec<u64>, batteries: usize) -> u64 {
+    match batteries {
+        1 => bank.into_iter().max().expect("Banken er tom"),
+        _ => {
+            let (max_position, max) = bank[0..bank.len() - (batteries - 1)]
+                .iter()
+                .enumerate()
+                .max_by_key(|&(pos, &value)| (value, std::cmp::Reverse(pos)))
+                .expect("Banken er for liten");
+            max * 10u64.pow(batteries as u32 - 1) + get_max_joltage(bank[max_position + 1..].to_vec(), batteries - 1)
         }
     }
-    let number =
-        selection.iter().fold(0u64, |acc, &d| acc * 10 + d as u64);
-    number
 }
 
 #[test]
