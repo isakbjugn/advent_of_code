23559575c5af886584fe60509497f6b07746dcd6
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-12-04 06:27:47 +0100

    4. desember, del 2

--- 2025/src/december_4.rs
+++ 2025/src/december_4.rs
@@ -17,7 +17,29 @@ pub fn part_1(input: &str) -> u64 {
 }
 
 pub fn part_2(input: &str) -> u64 {
-    0
+    let mut grid = Grid::from_str(input).unwrap();
+    let mut accessible_rolls = 0;
+    let mut removed_last_round = true;
+    let mut remove_list = Vec::new();
+
+    while removed_last_round {
+        removed_last_round = false;
+        for (cell, roll) in grid.iter() {
+            if roll != '@' { continue };
+
+            if grid.neighbor_iter(&cell).filter(|neighbor| grid.get(neighbor) == Some('@')).count() < 4 {
+                accessible_rolls += 1;
+                remove_list.push(cell);
+                removed_last_round = true;
+                continue
+            }
+        }
+        for &cell in &remove_list {
+            grid.set(cell, 'x');
+        }
+    }
+
+    accessible_rolls
 }
 
 #[test]
@@ -35,11 +57,11 @@ fn input_part_1() {
 #[test]
 fn sample_input_part_2() {
     let input = include_str!("../input/sample_4.txt");
-    //assert_eq!(part_2(input), 0)
+    assert_eq!(part_2(input), 43)
 }
 
 #[test]
 fn input_part_2() {
     let input = include_str!("../input/input_4.txt");
-    //assert_eq!(part_2(input), 0)
+    assert_eq!(part_2(input), 9243)
 }
