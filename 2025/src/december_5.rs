a80326d37ce8449080b09e35eab4acbc846b3bf5
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-12-05 06:09:21 +0100

    5. desember, del 1

--- 2025/src/december_5.rs
+++ 2025/src/december_5.rs
@@ -1,5 +1,20 @@
 pub fn part_1(input: &str) -> u64 {
-    0
+    let (ranges_str, ids_str) = input.split_once("\n\n").unwrap();
+    let ranges: Vec<(u64, u64)> = ranges_str
+        .lines()
+        .map(|line| {
+            let (start, inclusive_end) = line.split_once("-").unwrap();
+            (start.parse().unwrap(), inclusive_end.parse().unwrap())
+        })
+        .collect();
+    let ids: Vec<u64> = ids_str
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect();
+
+    ids.into_iter().filter(|id| {
+        ranges.iter().any(|(start, end)| id >= start && id <= end)
+    }).count() as u64
 }
 
 pub fn part_2(input: &str) -> u64 {
@@ -8,14 +23,14 @@ pub fn part_2(input: &str) -> u64 {
 
 #[test]
 fn sample_input_part_1() {
-    //let input = include_str!("../input/sample_5.txt");
-    //assert_eq!(part_1(input), 0)
+    let input = include_str!("../input/sample_5.txt");
+    assert_eq!(part_1(input), 3)
 }
 
 #[test]
 fn input_part_1() {
-    //let input = include_str!("../input/input_5.txt");
-    //assert_eq!(part_1(input), 0)
+    let input = include_str!("../input/input_5.txt");
+    assert_eq!(part_1(input), 623)
 }
 
 #[test]
