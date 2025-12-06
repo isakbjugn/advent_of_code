abbe050b1d68a08a3ffa978844ee26b3dc034343
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-12-06 07:48:21 +0100

    Oppsett dag 6

---
+++ 2025/src/december_6.rs
@@ -0,0 +1,31 @@
+pub fn part_1(input: &str) -> u64 {
+    0
+}
+
+pub fn part_2(input: &str) -> u64 {
+    0
+}
+
+#[test]
+fn sample_input_part_1() {
+    let input = include_str!("../input/sample_6.txt");
+    assert_eq!(part_1(input), 0)
+}
+
+#[test]
+fn input_part_1() {
+    let input = include_str!("../input/input_6.txt");
+    assert_eq!(part_1(input), 0)
+}
+
+#[test]
+fn sample_input_part_2() {
+    let input = include_str!("../input/sample_6.txt");
+    assert_eq!(part_2(input), 0)
+}
+
+#[test]
+fn input_part_2() {
+    let input = include_str!("../input/input_6.txt");
+    assert_eq!(part_2(input), 0)
+}
