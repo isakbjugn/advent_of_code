ac3c1f30977fe35713451f6e0abe718ac72705f7
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-12-07 09:18:56 +0100

    7. desember, del 1

--- 2025/src/december_7.rs
+++ 2025/src/december_7.rs
@@ -1,5 +1,33 @@
+use std::collections::HashSet;
+use crate::grid::Grid;
+use crate::position::Position;
+
 pub fn part_1(input: &str) -> u64 {
-    0
+    let tachyon_manifold = Grid::from_str(input).unwrap();
+    let start_position = tachyon_manifold.find('S').unwrap();
+
+    let mut beams = HashSet::from([start_position.x]);
+    let mut splits = 0;
+
+    for y in start_position.y..tachyon_manifold.height() {
+        let mut resulting_beams = HashSet::new();
+        for &beam_x in &beams {
+            match tachyon_manifold.get(&Position { x: beam_x, y: y + 1 }) {
+                Some('.') => {
+                    resulting_beams.insert(beam_x);
+                }
+                Some('^') => {
+                    resulting_beams.insert(beam_x - 1);
+                    resulting_beams.insert(beam_x + 1);
+                    splits += 1;
+                },
+                _ => { /* Strålen går utenfor rutenettet */ }
+            };
+        }
+        beams = resulting_beams;
+    }
+
+    splits
 }
 
 pub fn part_2(input: &str) -> u64 {
@@ -9,13 +37,13 @@ pub fn part_2(input: &str) -> u64 {
 #[test]
 fn sample_input_part_1() {
     let input = include_str!("../input/sample_7.txt");
-    assert_eq!(part_1(input), 0)
+    assert_eq!(part_1(input), 21)
 }
 
 #[test]
 fn input_part_1() {
     let input = include_str!("../input/input_7.txt");
-    assert_eq!(part_1(input), 0)
+    assert_eq!(part_1(input), 1675)
 }
 
 #[test]
