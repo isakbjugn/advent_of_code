9c3b95934b694d3d52e3e09486aa01952dd91d86
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-12-08 06:03:44 +0100

    Filer dag 8

--- 2025/src/december_8.rs
+++ 2025/src/december_8.rs
@@ -8,24 +8,24 @@ pub fn part_2(input: &str) -> u64 {
 
 #[test]
 fn sample_input_part_1() {
-    //let input = include_str!("../input/sample_8.txt");
+    let input = include_str!("../input/sample_8.txt");
     //assert_eq!(part_1(input), 0)
 }
 
 #[test]
 fn input_part_1() {
-    //let input = include_str!("../input/input_8.txt");
+    let input = include_str!("../input/input_8.txt");
     //assert_eq!(part_1(input), 0)
 }
 
 #[test]
 fn sample_input_part_2() {
-    //let input = include_str!("../input/sample_8.txt");
+    let input = include_str!("../input/sample_8.txt");
     //assert_eq!(part_2(input), 0)
 }
 
 #[test]
 fn input_part_2() {
-    //let input = include_str!("../input/input_8.txt");
+    let input = include_str!("../input/input_8.txt");
     //assert_eq!(part_2(input), 0)
 }
