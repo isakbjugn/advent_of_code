b823ace2eb8e50e6828c75b48f7dbd990629f2aa
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-12-10 15:03:04 +0100

    10. desember, del 2

--- 2025/src/december_10.rs
+++ 2025/src/december_10.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashSet, VecDeque};
+use good_lp::*;
 
 pub fn part_1(input: &str) -> u64 {
     input
@@ -59,8 +60,72 @@ fn press_button(lights: &Vec<char>, button: &Vec<u32>) -> Vec<char> {
     new_lights
 }
 
-pub fn part_2(input: &str) -> u64 {
-    0
+pub fn part_2(input: &str) -> u32 {
+    input
+        .lines()
+        .map(to_buttons_and_joltage)
+        .map(|(buttons, joltages)| solve_ilp(&buttons, &joltages))
+        .map(|solution| {
+            solution.iter().sum::<u32>()
+        })
+        .sum()
+}
+
+fn to_buttons_and_joltage(line: &str) -> (Vec<Vec<u32>>, Vec<u32>) {
+    let end_square_bracket_index = line.find("]").unwrap();
+    let (_, rest) = line.split_at(end_square_bracket_index + 1);
+    let start_curly_brace_index = rest.trim().find("{").unwrap();
+    let (button_str, joltage_str) = rest.trim().split_at(start_curly_brace_index);
+
+    let buttons: Vec<Vec<u32>> = button_str
+        .split_whitespace()
+        .map(|str|
+            str.chars().filter_map(|c| c.to_digit(10)).collect()
+        )
+        .collect();
+    let joltages: Vec<u32> = joltage_str
+        .trim_matches(|c| c == '{' || c == '}')
+        .split(',')
+        .filter_map(|s| s.trim().parse::<u32>().ok())
+        .collect();
+    let mut matrix_a = vec![vec![0; buttons.len()]; joltages.len()];
+    for (col, button) in buttons.iter().enumerate() {
+        for &row in button {
+            matrix_a[row as usize][col] = 1;
+        }
+    }
+
+    (matrix_a, joltages)
+}
+
+fn solve_ilp(matrix_a: &[Vec<u32>], vec_b: &[u32]) -> Vec<u32> {
+    let rows = matrix_a.len();
+    let cols = matrix_a[0].len();
+
+    let mut vars = variables!();
+    let mut x = Vec::new();
+
+    // Create non-negative integer variables in vector x
+    for _ in 0..cols {
+        x.push(vars.add(variable().integer().min(0)));
+    }
+
+    let objective: Expression = x.iter().copied().sum();
+
+    // Build constraints A * x = target
+    let mut pb = vars.minimise(objective).using(default_solver);
+
+    for r in 0..rows {
+        let expr: Expression = matrix_a[r].iter()
+            .enumerate()
+            .map(|(c, coef)| *coef as i32 * x[c])
+            .sum();
+
+        pb = pb.with(expr.eq(vec_b[r] as f64));
+    }
+
+    let solution = pb.solve().unwrap();
+    (0..cols).map(|col| solution.value(x[col]) as u32).collect()
 }
 
 #[test]
@@ -78,11 +143,11 @@ fn input_part_1() {
 #[test]
 fn sample_input_part_2() {
     let input = include_str!("../input/sample_10.txt");
-    assert_eq!(part_2(input), 0)
+    assert_eq!(part_2(input), 33)
 }
 
 #[test]
 fn input_part_2() {
     let input = include_str!("../input/input_10.txt");
-    assert_eq!(part_2(input), 0)
+    assert_eq!(part_2(input), 16978)
 }
