459bd7c11cad6032c1af980f4f352f1e75665bee
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-12-11 21:45:16 +0100

    9. desember, eksempel del 2

--- 2025/src/december_9.rs
+++ 2025/src/december_9.rs
@@ -28,7 +28,32 @@ fn get_rectangles_areas(tiles: &[Position]) -> Vec<u64> {
 }
 
 pub fn part_2(input: &str) -> u64 {
-    0
+    let mut tiles: Vec<_> = input.lines().map(to_tile).collect();
+    tiles.push(tiles[0]);
+
+    let edge_tiles = tiles
+        .windows(2)
+        .map(|w| (w[0], w[1]))
+        .flat_map(|(first_tile, second_tile)| first_tile.positions_between(&second_tile))
+        .collect::<Vec<_>>();
+
+    let mut areas = Vec::new();
+    for top_left in &tiles {
+        for bottom_right in tiles.iter().filter(|tile| tile.x > top_left.x && tile.y > top_left.y) {
+            let area = (bottom_right.x - top_left.x + 1) as u64 * (bottom_right.y - top_left.y + 1) as u64;
+
+            if !edge_tiles.iter().any(|tile| is_tile_inside_area(tile, top_left, bottom_right)) {
+                areas.push(area);
+            }
+        }
+    }
+
+    areas.into_iter().max().unwrap_or(0)
+}
+
+fn is_tile_inside_area(tile: &Position, top_left: &Position, bottom_right: &Position) -> bool {
+    tile.x > top_left.x && tile.x < bottom_right.x &&
+        tile.y > top_left.y && tile.y < bottom_right.y
 }
 
 #[test]
@@ -46,7 +71,7 @@ fn input_part_1() {
 #[test]
 fn sample_input_part_2() {
     let input = include_str!("../input/sample_9.txt");
-    assert_eq!(part_2(input), 0)
+    assert_eq!(part_2(input), 24)
 }
 
 #[test]
